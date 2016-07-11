63ba9589b3802a630b74261ba62dc3da5254b84b
koziscool <kozjob@yahoo.com>
2016-07-11 13:55:48 -0500

    bugfix no errors here

--- app/app.js
+++ app/app.js
@@ -1,9 +1,16 @@
 
-fido = angular.module('fideligard', ['ui-router']);
+fido = angular.module('fideligard', ['ui.router']);
 
 fido.config( function( $stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/stocks');
+  // $urlRouterProvider.otherwise('/stocks');
 
+  // $stateProvider
+
+  // .state( 'stocks', {
+  //   url: '/stocks',
+  //   controller: 'StocksCtrl',
+  //   templateUrl: '/app/templates/stocks.html',
+  // })
 
 });
